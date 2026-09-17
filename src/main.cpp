9d4b56d4216cf6e4c8fcf3c7ee72a9f02e94ec8a
VerazKnight <lemikula@gmail.com>
2026-09-17 02:06:28 -0400

    Added linux support and terminal input for selecting from multiple midi devices

--- src/main.cpp
+++ src/main.cpp
@@ -48,20 +48,22 @@ bool setup_glfw(const char* &glslVersion, GLFWwindow* &window) {
     return true;
 }
 
-void midi_callback( double deltatime, std::vector< unsigned char > *message, void *userData ) {
-  unsigned int nBytes = message->size();
-  // The message has 3 parts:
-  // The first byte appears to be either 144, signaling key pressed, or 128, signaling key release
-  // The second byte is the midi key number
-  // The third byte is the "velocity" v with witch the key was pressed, with v > 0 signaling a key press, and v = 0 signaling key release
-  // We will be using the second and third bytes and ignoring the first
-  std::string press_state = "";
-  if ((int)message->at(2) != 0) {
-    press_state = " Pressed";
-  } else {
-    press_state = " Released";
-  }
-  std::cout << "Midi Key: " << (int)message->at(1) << press_state << std::endl;
+void midi_callback(double deltatime, std::vector<unsigned char> *message, void *userData) {
+    // Some messages contain less than 3 parts, and if they do, we can ignore them because they are not a keypress event
+    if (!message || message->size() < 3) return;
+    unsigned int nBytes = message->size();
+    // The message has 3 parts:
+    // The first byte appears to be either 144, signaling key pressed, or 128, signaling key release
+    // The second byte is the midi key number
+    // The third byte is the "velocity" v with witch the key was pressed, with v > 0 signaling a key press, and v = 0 signaling key release
+    // We will be using the second and third bytes and ignoring the first
+    std::string press_state = "";
+    if ((int)message->at(2) != 0) {
+        press_state = " Pressed";
+    } else {
+        press_state = " Released";
+    }
+    std::cout << "Midi Key: " << (int)message->at(1) << press_state << std::endl;
 }
 
 int main() {
@@ -85,17 +87,52 @@ int main() {
     bool running = true;
 
     // rtmidi
-    RtMidiIn *midiin = new RtMidiIn();
+    RtMidiIn *midiin = nullptr;
+    try {
+        midiin = new RtMidiIn();
+    }
+    catch ( RtMidiError &error ) {
+        error.printMessage();
+        exit( EXIT_FAILURE );
+    }
     
     // Check available ports.
     unsigned int nPorts = midiin->getPortCount();
-    if ( nPorts == 0 ) {
-        std::cout << "No ports available!\n";
-        return 1;
+    std::cout << "\nThere are " << nPorts << " MIDI input sources available.\n";
+    if (nPorts == 0) {
+        std::cerr << "No MIDI ports available" << std::endl;
     }
     
-    midiin->openPort( 0 );
-    
+    for ( unsigned int i=0; i<nPorts; i++ ) {
+        try {
+            std::cout << "  Input Port #" << i + 1 << ": " << midiin->getPortName(i) << '\n';;
+        }
+        catch ( RtMidiError &error ) {
+            error.printMessage();
+            return 1;
+        }
+    }
+
+    unsigned int port = 0;
+
+    if (nPorts > 1) {
+        while (true) {
+            std::cout << "Select MIDI Device [1-" << (nPorts) << "]: ";
+            std::cin >> port;
+
+            if (port >= 1 && port <= nPorts) {
+                port -= 1;
+                break;
+            }
+            
+            std::cout << "Please select a valid device" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(100000, '\n');
+        }
+    }
+
+    midiin->openPort(port);
+        
     // Set our callback function.  This should be done immediately after
     // opening the port to avoid having incoming messages written to the
     // queue.
